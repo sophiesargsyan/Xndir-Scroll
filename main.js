211a70695fc635bca002afcd436432a08396bad9
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-06-02 16:50:10 +0400

    Step 2:Created 2 div and function for them

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import { Browser } from './browser.js';
+import { moveToCenter } from './moveToCenter.js';
 
+
+// Call functions from modules
 const browser = new Browser();
 const scrollWidth = browser.calcScrollWidth();
 console.log('Browser Scroll Width:', scrollWidth);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const parent = document.getElementById('parent');
+    const nested = document.getElementById('nested');
+    moveToCenter(parent, nested);
+});
